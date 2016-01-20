0b117ffb90448fadf77fa7144c92848fd966a278
jkrstulo <jakov.krstulovic@gmail.com>
2016-01-20 16:01:13 +0100

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,5 +28,8 @@ urlpatterns = [
 	url(r'^search-form/$', book_views.search_form),
 	url(r'^search/$', book_views.search),
 	url(r'^contact/$', views.contact),
-	url(r'^$', blog_views.post_list, name = 'post_list')
+	url(r'^$', blog_views.post_list, name = 'post_list'),
+	url(r'^post/(?P<pk>[0-9]+)/$', blog_views.post_detail, name = 'post_detail'),
+	url(r'^post/new/$', blog_views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', blog_views.post_edit, name='post_edit'),
 ]
